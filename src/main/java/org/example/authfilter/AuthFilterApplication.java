c42bf8b04da1b069a676b267b8fdfd4f9635836e
Nhahan <kisy324@naver.com>
2024-08-31 18:54:06 +0900

    first commit

---
+++ src/main/java/org/example/authfilter/AuthFilterApplication.java
@@ -0,0 +1,13 @@
+package org.example.authfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/authfilter/AuthFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
